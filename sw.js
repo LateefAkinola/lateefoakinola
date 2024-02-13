1b3bee184a45a3a83615fee8dddfcef5310ebe0d
lateef <akinolalateefolanrewaju@gmail.com>
2024-02-13 10:13:26 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-99bd26797e4d1dd0d31d.js"
+    "url": "webpack-runtime-b030d0bc0c4e43fd0d04.js"
   },
   {
     "url": "framework-636420dd6be07fc61d88.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "133171a427ff1c2294683cbb392244de"
+    "revision": "daaef496d8d2bebc5927b06b7884669e"
   },
   {
     "url": "polyfill-fd82e6b942a7ace3128b.js"
